24226fbbe0226c7051575fa2fedc8128703ec569
Wesley Adriann <wesleyadriann@gmail.com>
2025-04-25 10:30:33 -0300

    book part 5.1
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- 5 Using Structs to Structure Related Data/1 Defining and Instantiating Structs/main.rs
+++ 5 Using Structs to Structure Related Data/1 Defining and Instantiating Structs/main.rs
@@ -5,15 +5,35 @@ struct User {
   active: bool,
 }
 
+struct Color(u32, u32, u32);
+struct Point(i32, i32, i32);
+
+struct AlwaysEqual;
+
 fn main() {
   let mut user1 = User {
-    username: String::from("foobaa"),
-    email: String::from("foo@baa.baz"),
+    username: String::from("user1"),
+    email: String::from("user1@baa.baz"),
     sign_in_count: 1,
     active: true,
   };
 
   user1.email = String::from("baz@foo.baa");
+
+  let user2 = User {
+    email: String::from("user2@baa.baz"),
+    ..user1
+  };
+
+  let user3 = create_user(
+    "user3@baa.baz".to_string(),
+    "user3".to_string()
+  );
+
+  let black = Color(0, 0, 0);
+  let origin = Point(0, 0, 0);
+
+  let subject = AlwaysEqual;
 }
 
 fn create_user(email: String, username: String) -> User {
@@ -24,3 +44,4 @@ fn create_user(email: String, username: String) -> User {
     sign_in_count: 1,
   }
 }
+
