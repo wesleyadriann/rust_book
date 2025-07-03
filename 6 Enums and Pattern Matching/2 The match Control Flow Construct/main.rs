44445c0af0d142eb36e67c31f1cae0bf5f017312
Wesley Adriann <wesleyadriann@gmail.com>
2025-07-03 18:36:23 -0300

    book part 6.2
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- 6 Enums and Pattern Matching/2 The match Control Flow Construct/main.rs
+++ 6 Enums and Pattern Matching/2 The match Control Flow Construct/main.rs
@@ -1,8 +1,14 @@
+#[derive(Debug)]
+enum UsState {
+    Alaska,
+    Alabama,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
@@ -11,6 +17,26 @@ fn main() {
     let cents = value_in_cents(coin);
 
     println!("{}", cents);
+
+    let alaska_quarter = Coin::Quarter(UsState::Alaska);
+
+    value_in_cents(alaska_quarter);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => println!("three"),
+        7 => println!("seven"),
+        other => println!("The value is {other}"),
+    }
+
+    match dice_roll {
+        1 => println!("one"),
+        _ => (),
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -21,6 +47,16 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
